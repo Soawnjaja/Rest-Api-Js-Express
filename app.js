d50f9ab0ceca572ea013d78d89b9db40768bce5a
SoawnJaja <basstraper@gmail.com>
2022-07-05 00:25:34 +0300

    updated method GET working

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const path = require('path')
 const app = express()
 
 
-
-
+const CONTACTS = [
+  {id: 1, name: "NewName", value: "7-908-038-55-22", marked: false}
+]
+// GET
+app.get('/api/contacts', (req,res) => {
+  res.status(200).json(CONTACTS)
+})
 
 // ----> Запросы должны отрабатывать посденими поэтому находятся в низу
  // статика нужна для того, что бы найти пути завязаные под html
--- client/frontend.js
+++ client/frontend.js
@@ -1,25 +1,39 @@
 
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js'
 
-
+Vue.component('loader', {
+  template: `
+  <div style = "display: flex; justify-content: center; align-items: center;>
+  <div class="spinner-border" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>
+  </div>
+  `
+})
 new Vue({
   el: '#app',
   data(){
     return {
+      loading : false,
       form: {
         name: '',
         value: ''
       },
       contacts: [
-        {id: 1, name: "NewName", value: "7-908-038-55-22", marked: false}
+       
       ]
+    }
+  },
+  computed: {
+    canCreate() {
+      return this.form.name.trim() && this.form.value.trim()
     }
   },
     methods: {
       createContact() {
       const {...contact} = this.form
 
-      this.contacts.push({...contact, id: Date.now()})
+      this.contacts.push({...contact, id: Date.now(), marked: false})
       
       this.form.name = this.form.value = ''
     },
@@ -28,7 +42,34 @@ new Vue({
       contact.marked = true
     },
     removeContact(id){
-
+         this.contacts = this.contacts.filter(c => c.id !== id )
     }
+    }, 
+    async mounted() {
+      this.loading = true
+     this.contacts = await request('/api/contacts')
+     this.loading = false
+      
     } 
 })
+
+async function request(url, method = 'GET', data = null) {
+  try {
+    const headers = {}
+    let body
+
+    if (data) {
+      headers['Content-Type'] = 'application/json'
+      body = JSON.stringify(data)
+    }
+
+    const response = await fetch(url, {
+      method,
+      headers,
+      body
+    })
+    return await response.json()
+  } catch (e) {
+    console.warn('Error:', e.message)
+  }
+}
